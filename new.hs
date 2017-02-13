aa9882c811bc4ae8033a9c9bd1d990a6a80bf146
sudsj <sudhanshuj007@gmail.com>
2017-02-14 01:28:14 +0530

    Added haskell functions file

---
+++ new.hs
@@ -0,0 +1,12 @@
+doubleMe x = x+x
+doubletwo x y = doubleMe x + doubleMe y + 9
+doubleBig x = if x > 100 then x*2 else x
+doubleBig' x = (if x > 100 then x*2 else x) + 2
+
+
+b = take 10 [5,8..305]
+
+evens :: [Integer] -> [[Char]] 
+evens s = [if x < 10 then "Small even" else "Big even" | x <- s,  not (odd x)]
+
+
