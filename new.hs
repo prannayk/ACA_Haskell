0897dc56ef5a727576a331701a889bb72577ddde
sudsj <sudhanshuj007@gmail.com>
2017-02-15 03:52:36 +0530

    Added a function

--- new.hs
+++ new.hs
@@ -12,3 +12,16 @@ evens s = [if x < 10 then "Small even" else "Big even" | x <- s,  not (odd x)]
 
 onlyUpper :: [Char] -> [Char]
 onlyUpper st = [x|x <- st,x `elem` ['A'..'Z']]
+
+doYouGAF :: [[Char]] -> [[Char]] 
+doYouGAF st = [if (x == "No" || x== "no") then "No Fs given" else "But why" | x <-  st, x == head st]
+
+ 
+HoursYouReadBooks :: (Integral a) => a -> a -> String
+
+HoursYouReadBooks hours books
+  | ratio <= 1 = "C'mmon give those books some time"
+  | ratio <= 2 = "That's quite good!"
+  | ratio <= 3 = "Oh you are a devotee"
+  | otherwise = "Get OUT! Like Seriously!"
+  where ratio = (fromIntegral (hours) / books)
