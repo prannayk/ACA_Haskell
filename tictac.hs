c97ada3a2c1d039dcef0d01da115d204354076d8
sudsj <sudhanshuj007@gmail.com>
2017-03-13 00:24:23 +0530

    Added timeIt modules

--- tictac.hs
+++ tictac.hs
@@ -2,9 +2,9 @@ import Data.Char
 import Data.List
 import System.IO
 import Data.Int
+import System.TimeIt
 
 -- Reading a number
-
 getNum :: String -> IO Int
 getNum prompt = do putStr prompt
                    xs <- getLine
@@ -14,9 +14,7 @@ getNum prompt = do putStr prompt
                       do putStrLn "NOT A VALID NUMBER"
                          getNum prompt
 
-
-size :: Int
-size = 3
+--setting up grid and turn changer
 
 type Playgrid = [[Player]]
 
@@ -29,74 +27,62 @@ changeturn B = B
 changeturn X = O
 
 
-blank :: Playgrid --creates blank Playgrid
-blank = replicate size (replicate size B)
+blank :: Int -> Playgrid --creates blank Playgrid
+blank num = replicate num (replicate num B)
 
 full :: Playgrid -> Bool--will check for full Playgrid
 full = all (/= B) . concat
 
-wins :: Player -> Playgrid -> Bool--checks for win condition
-wins p g = any checkr (rows ++ cols ++ dias)--metrue if any return true
+wins :: Int -> Player -> Playgrid -> Bool--checks for win condition
+wins nums p g = any checkr (rows ++ cols ++ dias)--true if any return true
            where
               checkr = all (== p)
               rows = g
               cols = transpose g
-              dias = [findDiag g, findDiag (map reverse g)]
+              dias = [findDiag nums g, findDiag nums (map reverse g)]
 
-findDiag :: Playgrid -> [Player]
-findDiag g = [g !! n !! n | n <- [0..size-1]]
+findDiag :: Int -> Playgrid -> [Player]
+findDiag nums g = [g !! n !! n | n <- [0..nums-1]]
 
 -- !!Plan to add gridlines to the Playgrid output, thus two seperate row and player str of strings!! 
-
+--functions to print the grid 
 putPlaygrid :: Playgrid -> IO ()
-putPlaygrid =
-   putStrLn . unlines . concat .map rowStr--unline inserts new line character
-
-rowStr :: [Player] -> [String]
-rowStr = beside . map playerStr
-          where
-             beside = foldr1 (zipWith (++)) -- joins the strings from last two elements 
-
-playerStr :: Player -> [String]
-playerStr O = ["   ", " O ", "   "]
-playerStr B = ["   ", "   ", "   "]
-playerStr X = ["   ", " X ", "   "]
-
+putPlaygrid = mapM_ (putStrLn . intercalate " " . map show) 
 -- For moves 
 
-isValid :: Playgrid -> Int -> Bool
-isValid g i = (i >= 0) && (i < (size^2)) && (concat g !! i == B)
+isValid :: Int -> Playgrid -> Int -> Bool
+isValid nums g i = (i >= 0) && (i < (nums^2)) && (concat g !! i == B)
 
-move:: Playgrid -> Int -> Player -> [Playgrid]
-move g i p =
-   if isValid g i then [cutt size (xs ++ [p] ++ ys)] else []
+move:: Int -> Playgrid -> Int -> Player -> [Playgrid]
+move nums g i p =
+   if isValid nums g i then [cutt nums (xs ++ [p] ++ ys)] else []
    where (xs,B:ys) = splitAt i (concat g)--splitAt splits list at i
 
 cutt :: Int -> [a] -> [[a]]--creates sublists of size n from original lists
 cutt n [] = []
 cutt n xs = take n xs : cutt n (drop n xs)
 
-
-
-tictactoe :: IO ()
-tictactoe = do gameloop blank O
-
-gameloop :: Playgrid -> Player -> IO ()
-gameloop g p = do putPlaygrid g
-                  gameloop' g p
-
-gameloop' :: Playgrid -> Player -> IO ()
-gameloop' g p 
-         | wins O g  = putStrLn "Player O is winner!\n"
-         | wins X g  = putStrLn "Player X is winner!\n"
+--main function to take in grid size and runs gameloop
+main :: IO ()
+main = do sizes <- getNum "Enter the grid size : "
+          timeIt $ gameloop sizes (blank sizes) O
+--prints the playGrid and calls gameloop'
+gameloop :: Int -> Playgrid -> Player -> IO ()
+gameloop sizes g p = do putPlaygrid g
+                        gameloop' sizes g p
+--checks game terminate conditions and continues game if not true
+gameloop' :: Int -> Playgrid -> Player -> IO ()
+gameloop' sizes g p 
+         | wins sizes O g  = putStrLn "Player O is winner!\n"
+         | wins sizes X g  = putStrLn "Player X is winner!\n"
          | full g    = putStrLn "GAME DRAW!\n"
          | otherwise =
               do num <- getNum (prompts' p)
-                 case move g num p of
+                 case move sizes g num p of
                     []   -> do putStrLn "Invalid Move"
-                               gameloop' g p
-                    [g'] -> gameloop g' (changeturn p)
-
+                               gameloop' sizes g p
+                    [g'] -> gameloop sizes g' (changeturn p)
+--prompts user to enter move
 prompts' :: Player -> String
 prompts' p = "Player " ++ show p ++ ", enter your move: "
 
