26f228ddc32c6c17958f7facdce7b9b51b77c804
Siddhant Kar <siddhk@iitk.ac.in>
2017-03-18 23:40:10 +0530

    Added it

---
+++ TicTacToe.hs
@@ -0,0 +1,34 @@
+row :: Char -> Int -> String
+row _ 0 = []
+row x n = row x (n-1) ++ [x]
+
+matrix :: Int -> Int -> [String]
+matrix 0 _ = []
+matrix m n = matrix (m-1) n ++ [row '.' n]
+
+start n = matrix n n
+
+
+
+rowdisplay :: String -> String
+rowdisplay [] = []
+rowdisplay (x:xs) = " " ++ [x] ++ rowdisplay xs
+
+display :: [String] -> String
+display [] = []
+display (x:xs) = rowdisplay x ++ "\n" ++ display xs
+
+show xs = putStr (display xs)
+
+
+
+replace :: [String] -> Int -> Int -> Char -> [String]
+replace xs i j x = let (as, b:bs) = splitAt (i-1) xs
+                       (cs, _:ds) = splitAt (j-1) b
+                   in as ++ [cs ++ [x] ++ ds] ++ bs
+
+x :: Int -> Int -> [String] -> [String]
+x i j xs = replace xs i j 'x'
+
+o :: Int -> Int -> [String] -> [String]
+o i j xs = replace xs i j 'o'
